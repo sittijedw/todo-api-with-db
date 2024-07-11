fae59b005e40fd978967da00d4b31d1f24120256
Sittijed Wongtichawat <sittijed.w@arise.tech>
2024-07-11 23:56:06 +0700

    Write test case for get all todos

---
+++ e2e/api/todo.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Get todos', () => {
+  test('should response all todos when request /api/v1/todos', async ({
+    request,
+  }) => {
+    const postResponse1 = await request.post('http://localhost:8910/api/v1/todos',
+      {
+        data: {
+          title: 'Learn Go',
+          status: 'active'
+        }
+      }
+    )
+    const postResponse2 = await request.post('http://localhost:8910/api/v1/todos',
+      {
+        data: {
+          title: 'Learn Java',
+          status: 'inactive'
+        }
+      }
+    )
+    const reps = await request.get('http://localhost:8910/api/v1/todos')
+
+    expect(reps.ok()).toBeTruthy()
+    expect(await reps.json()).toEqual(
+      expect.arrayContaining([
+        {
+          id: expect.any(Number),
+          title: 'Learn Go',
+          status: 'active'
+        },
+        {
+          id: expect.any(Number),
+          title: 'Learn Java',
+          status: 'inactive'
+        }
+      ]
+      )
+    )
+
+    const postResponseJson1 = await postResponse1.json()
+    const postResponseJson2 = await postResponse2.json()
+    const postResponseID1 = postResponseJson1['id']
+    const postResponseID2 = postResponseJson2['id']
+
+    await request.delete('http://localhost:8910/api/v1/todos/' + String(postResponseID1))
+    await request.delete('http://localhost:8910/api/v1/todos/' + String(postResponseID2))
+  })
+})
